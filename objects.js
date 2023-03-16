38a0b8db53d03f874cc05eebfcf4f722aa87451a
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-16 11:23:12 -0700

    practice

---
+++ objects.js
@@ -0,0 +1,15 @@
+let hobbit = {
+    firstName: "Frodo",
+    lastName: "Baggins",
+    age: 51,
+  };
+
+
+//   DOT NOtation
+console.log(hobbit.firstName);
+console.log(`${hobbit.firstName} ${hobbit.lastName} is ${hobbit.age} years old.`);
+
+// square bracket notation
+console.log(hobbit[firstName]);
+console.log(hobbit["firstName"]);
+console.log(`$hobbit["firstName]`)
---
+++ script.js
@@ -0,0 +1,22 @@
+const arr1 = new Array(6);
+arr1[0]= "rick"
+arr1[1]= "morty"
+arr1[2]= "robot"
+arr1[3]= "family guy"
+arr1[4]= 2021
+arr1[4]=2022
+arr1.push("newvalue")
+for (let i = 0; i < arr1.length; i++){
+    console.log(i);
+    console.log(arr1[i]);
+}
+
+
+const arr2= []
+
+arr2[0]= "rick"
+arr2[1]= "morty"
+arr2[2]= "robot"
+arr2[3]= "family guy"
+arr2[4]= 2021
+// console.log(arr2);
