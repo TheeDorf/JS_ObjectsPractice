896f8bbffab30d4bfe66e6abd43a96284316a0b5
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-17 07:02:42 -0700

    update

--- objects.js
+++ objects.js
@@ -1,15 +1,33 @@
-let hobbit = {
-    firstName: "Frodo",
-    lastName: "Baggins",
-    age: 51,
-  };
+// let hobbit = {
+//     firstName: "Frodo",
+//     lastName: "Baggins",
+//     age: 51,
+//   };
 
 
-//   DOT NOtation
-console.log(hobbit.firstName);
-console.log(`${hobbit.firstName} ${hobbit.lastName} is ${hobbit.age} years old.`);
+// //   DOT NOtation
+// console.log(hobbit.firstName);
+// console.log(`${hobbit.firstName} ${hobbit.lastName} is ${hobbit.age} years old.`);
 
-// square bracket notation
-console.log(hobbit[firstName]);
-console.log(hobbit["firstName"]);
-console.log(`$hobbit["firstName]`)
+// // square bracket notation
+// console.log(hobbit[firstName]);
+// console.log(hobbit["firstName"]);
+// console.log(`$hobbit["firstName]`)
+
+
+const arr3=[23,45,32,65,78]
+let sum = 0;
+for (let i=0; i <arr3.length; i++){
+    // sum = sum+arr3[i]  
+    sum+= arr3[i];
+    console.log(sum);
+}
+
+arr3.forEach( ()=>{} )
+arr3.reduce(()=>{})
+
+
+
+const book = {
+    
+}
